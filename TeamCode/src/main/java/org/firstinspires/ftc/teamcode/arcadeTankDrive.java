5dc8dcdd39dcdfb4d927e77b88f908dbae85c270
TRXAlpha <christianmorogan@gmail.com>
2026-08-27 10:53:06 +0300

    commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/arcadeTankDrive.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name="teleop tank arcade", group="tank")
+public class arcadeTankDrive extends OpMode {
+    private DcMotor stanga, dreapta;
+    @Override
+    public void init() {
+        stanga  = hardwareMap.get(DcMotor.class, "stanga");
+        dreapta = hardwareMap.get(DcMotor.class, "dreapta");
+
+        // Reverse the left motor so forward stick moves the robot forward
+        stanga.setDirection(DcMotor.Direction.REVERSE);
+        dreapta.setDirection(DcMotor.Direction.FORWARD);
+    }
+    @Override
+    public void start(){
+
+    }
+    @Override
+    public void loop(){
+
+        // Read joysticks (negated because pushing forward is negative by default)
+        double putereStanga  = -gamepad1.left_stick_y;
+        double putereDreapta = -gamepad1.right_stick_y;
+
+        stanga.setPower(putereStanga);
+        dreapta.setPower(putereDreapta);
+
+        telemetry.addData("Motors", "Left (%.2f), Right (%.2f)", putereStanga, putereDreapta);
+        telemetry.update();
+    }
+}
+
